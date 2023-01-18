ab5e84fd122aef6aeb51fd36b896a92694a26917
soohyoen <gnejfejf789@naver.com>
2023-01-18 13:49:29 +0900

    activation function, loss function 바꿈, Readme 참조

---
+++ Grid_world(custom)/custom_env(deep_sarsa)/run/fifth/README.md
@@ -0,0 +1,42 @@
+발산하는 이유 :
+
+gradient loss → sigmoid함수 (gradient 가 0이 되기때문에 vanishing gradient)
+              → sigmoid를 썼을때 아주작은 값을 제곱하면 더 작은값이 되어버려 저속 수렴문제가 발생
+              → mse는 단순히 거리를 비교하며, cross entropy는 두개의 확률분포간의 거리를 비교
+
+→ activation function : softmax, loss function : cross entropy
+→  learning_rate 가 너무 낮으면 발산가능성
+→  dropout : depth가 깊어지면 overfiting가능성 이를 사용하여 과적합을 막음
+
+이전 상황 :
+Dropout을 사용하니 애초에 수렴이 안되는 문제가 발생
+
+시도 2:
+● activation function : softmax, loss function : cross entropy 사용
+
+기존 relu에서 softmax로 바꿈.
+
+loss function mse → cross entropy
+
+mse : 평균 제곱 오차 손실
+        → 신경망의 출력과 타겟이 연속인 회귀 문제에서 널리 사용하는 손실함수
+          예 측과 타겟값의 차이를 제곱하여 평균한 값
+ 
+categorical_crossentropy : 범주형 교차 엔트로피
+                           → 출력을 클래스 소속 확률에 대한 예측으로 이해할 수 있는 문제에서 사용
+                              (레이블(y) 클래스가 2개 이상일 경우 사용, 멀티클래스 분류에 사용)
+                           → 활성화 함수는 softmax(벡터의 요소값에서 0~1사이값에서 모든 합이 1이 됨)
+
+이론상으로 cross entropy는 맞아 보이지않음. 실제 학습 결과로 전혀 수렴하지 않음.
+
+last layer가 softmax였네.. 이전 Dense는 relu와 마지막 Dense는 softmax로 바꿔서 학습
+
+softmax : 출력값이 N개
+          입력값을 각각 지수함수로 취하고, 이를 정규화
+          정규화로 인해 각 출력값은 0~1 값을 가짐
+          모든 출력값의 합은 반드시 1
+          N가지 중 한가지에 속할 확률 표현 가능(=Multi-class Classification)
+          softmax를 사용하는 경우 결과를 확률로 변경하며 결과가 분류를 해야되는 것이라면 softmax를 
+          써서 모든 결과가 1로 수렴하게 만들 수 있음.
+
+결과적으로 cross entropy와 softmax를 쓰니까 수렴함. 학습결과도 좋음.. 
---
+++ Grid_world(custom)/custom_env(deep_sarsa)/run/fifth/run.py
@@ -0,0 +1,94 @@
+import copy
+import pylab
+import random
+import numpy as np
+from environment import Env
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.models import Sequential
+
+EPISODES =10
+
+class DeepSARSAgent:
+    def __init__(self):
+        self.load_model = True
+        self.action_space = [0, 1, 2, 3, 4]
+        self.action_size = len(self.action_space)
+        self.state_size = 39
+        self.discount_factor = 0.99
+        self.learning_rate = 0.001
+        
+        self.epsilon = 1.
+        self.epsilon_decay = .9999
+        self.epsilon_min = 0.01
+        self.model = self.build_model()
+        
+        if self.load_model:
+            self.epsilon = 0.05
+            self.model.load_weights('./save_model/deep_sarsa.h5')
+            
+    
+    def build_model(self):
+        model = Sequential()
+        model.add(Dense(60, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(60, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.summary()
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+    
+    def get_action(self,state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        else:
+            state = np.float32(state)
+            q_values = self.model.predict(state)
+            return np.argmax(q_values[0])
+        
+    def train_model(self,state, action, reward, next_state, next_action, done):
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+            
+            state = np.float32(state)
+            next_state = np.float32(next_state)
+            target = self.model.predict(state)[0]
+            
+            if done:
+                target[action] = reward
+            
+            else:
+                target[action] = (reward+ self.discount_factor *
+                                 self.model.predict(next_state[0][next_action]))
+                
+            target = np.reshape(target, [1,5])
+            self.model.fit(state, target, epochs=1, verbose=0)
+
+if __name__ == "__main__":
+    env = Env()
+    agent = DeepSARSAgent()
+  
+    global_step = 0
+    scores, episodes = [], []
+
+    for e in range(EPISODES):
+        done = False
+        score = 0
+        state = env.reset()
+        state = np.reshape(state, [1, 39])
+
+        while not done:
+            global_step += 1
+
+            action = agent.get_action(state)
+            next_state, reward, done = env.step(action)
+            next_state = np.reshape(next_state, [1,39])
+            next_action = agent.get_action(next_state)
+            state = next_state
+            score += reward
+
+            if done:
+                scores.append(score)
+                episodes.append(e)
+                print("episode:", e, "  score:", score, "global_step",
+                      global_step, "  epsilon:", agent.epsilon)
+
